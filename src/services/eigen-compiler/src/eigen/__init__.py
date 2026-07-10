128d4b418901072b5eb7026307dbae77e5c3ea7d
NYankovich <86417114+NYankovich@users.noreply.github.com>
2026-07-10 15:27:10 +0700

    Fix system API integration test startup

---
+++ src/services/eigen-compiler/src/eigen/__init__.py
@@ -0,0 +1,5 @@
+"""Eigen namespace package for generated service APIs."""
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
--- src/services/neuro-symbolic-service/src/eigen/__init__.py
+++ src/services/neuro-symbolic-service/src/eigen/__init__.py
@@ -1,1 +1,5 @@
-"""Internal neuro-symbolic service package."""
+"""Eigen namespace package for generated service APIs."""
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
